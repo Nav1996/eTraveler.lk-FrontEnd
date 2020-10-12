d7d26c80a0b3a143bb845a5068a1979d20374235
Dinuka Sankalpa <dinuka.caldera@gmail.com>
2020-10-12 22:42:51 +0530

    Package Frontend

---
+++ src/app/registeredPackage/registered-package/registered-package.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {DashboardService} from './../../services/dashboard.service';
+import {AuthService} from './../../services/auth.service';
+
+@Component({
+  selector: 'app-registered-package',
+  templateUrl: './registered-package.component.html',
+  styleUrls: ['./registered-package.component.css']
+})
+export class RegisteredPackageComponent implements OnInit {
+  currentUser:any;
+  userType:any
+  registeredPackage:any;
+
+  constructor(private dashBoardServices:DashboardService,private authServices:AuthService) { 
+    this.authServices.currentUser.subscribe(x=>this.currentUser=x)
+    this.authServices.currentUserType.subscribe(x=>this.userType=x)
+    
+    
+    
+    
+    
+    
+  }
+
+  ngOnInit(): void {
+    this.dashBoardServices.getAllRegisteredPackage(this.currentUser.username)
+    .subscribe((data)=>{
+      this.registeredPackage=data
+    })
+    
+  }
+
+}
